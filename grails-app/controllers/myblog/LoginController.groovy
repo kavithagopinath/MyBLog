dbdb4ae760de0279f78f5f7ff47793fdd91a5c71
Joel Sierra <jsierra@student.cscc.edu>
2016-11-29 10:23:18 -0500

    added spring security and generated controllers

--- grails-app/controllers/myblog/LoginController.groovy
+++ grails-app/controllers/myblog/LoginController.groovy
@@ -1,8 +1,104 @@
 package myblog
 
+
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
 class LoginController {
 
-    def scaffold = true
-	
-	
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Login.list(params), model:[loginInstanceCount: Login.count()]
+    }
+
+    def show(Login loginInstance) {
+        respond loginInstance
+    }
+
+    def create() {
+        respond new Login(params)
+    }
+
+    @Transactional
+    def save(Login loginInstance) {
+        if (loginInstance == null) {
+            notFound()
+            return
+        }
+
+        if (loginInstance.hasErrors()) {
+            respond loginInstance.errors, view:'create'
+            return
+        }
+
+        loginInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'login.label', default: 'Login'), loginInstance.id])
+                redirect loginInstance
+            }
+            '*' { respond loginInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Login loginInstance) {
+        respond loginInstance
+    }
+
+    @Transactional
+    def update(Login loginInstance) {
+        if (loginInstance == null) {
+            notFound()
+            return
+        }
+
+        if (loginInstance.hasErrors()) {
+            respond loginInstance.errors, view:'edit'
+            return
+        }
+
+        loginInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Login.label', default: 'Login'), loginInstance.id])
+                redirect loginInstance
+            }
+            '*'{ respond loginInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Login loginInstance) {
+
+        if (loginInstance == null) {
+            notFound()
+            return
+        }
+
+        loginInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Login.label', default: 'Login'), loginInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'login.label', default: 'Login'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
